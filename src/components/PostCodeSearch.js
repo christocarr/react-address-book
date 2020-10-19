e4810500145ae9f8a86c49921bd2a2822b545978
christocarr <christocarr@outlook.com>
2020-10-19 17:16:06 +0100

    Feature: select component with options from postcode search

--- src/components/PostCodeSearch.js
+++ src/components/PostCodeSearch.js
@@ -2,21 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 function PostCodeSearch() {
 	const [search, setSearch] = useState('');
+	const [addresses, setAddresses] = useState([])
 
 	function handleSubmit(e) {
 		e.preventDefault();
+
 		const APIKEY = 'HF5s4vq7pEupvXLhw7uCWg28754';
-		const addresses = async () => {
+
+		const getAddresses = async () => {
 			let res = await fetch(
 				`https://api.getAddress.io/find/${search}?api-key=${APIKEY}`
 			);
-			console.log(res);
 			let data = await res.json();
-			console.log(data);
+			console.log(data.addresses);
+			setAddresses(data.addresses)
 		};
 
-		addresses();
+		getAddresses()
 	}
+
+	useEffect(() => {
+
+	}, [])
 
 	return (
 		<div>
@@ -27,6 +34,13 @@ function PostCodeSearch() {
 					type="text"
 				/>
 				<button>Search</button>
+				{addresses.length > 0 &&
+					<select>
+						{addresses.map((v, i) => (
+							<option key={i}>{v}</option>
+						))}
+					</select>
+				}
 			</form>
 		</div>
 	);
