bbc3f76964823bf0760940e60e3898cda3d09546
christocarr <christocarr@outlook.com>
2020-10-20 17:32:35 +0100

    Add missing form inputs

--- src/components/AddContactForm.js
+++ src/components/AddContactForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function AddContactForm({ handleSubmit, handleChange, newContact, postcode }) {
+function AddContactForm({ handleSubmit, handleChange, newContact }) {
 	return (
 		<form onSubmit={handleSubmit}>
 			<label htmlFor="name">Name</label>
@@ -9,8 +9,8 @@ function AddContactForm({ handleSubmit, handleChange, newContact, postcode }) {
 				value={newContact.name}
 				name="name"
 				type="text"
-        onChange={handleChange}
-        placeholder="name"
+				onChange={handleChange}
+				placeholder="name"
 			/>
 			<label htmlFor="firstLine">Address Line One</label>
 			<input
@@ -18,8 +18,8 @@ function AddContactForm({ handleSubmit, handleChange, newContact, postcode }) {
 				value={newContact.addressLineOne}
 				name="addressLineOne"
 				type="text"
-        onChange={handleChange}
-        placeholder="first line"
+				onChange={handleChange}
+				placeholder="first line"
 			/>
 			<label htmlFor="secondLine">Address Line Two</label>
 			<input
@@ -27,8 +27,8 @@ function AddContactForm({ handleSubmit, handleChange, newContact, postcode }) {
 				value={newContact.addressLineTwo}
 				name="secondLine"
 				type="text"
-        onChange={handleChange}
-        placeholder="second line"
+				onChange={handleChange}
+				placeholder="second line"
 			/>
 			<label htmlFor="city">Town/City</label>
 			<input
@@ -36,26 +36,44 @@ function AddContactForm({ handleSubmit, handleChange, newContact, postcode }) {
 				value={newContact.city}
 				name="city"
 				type="text"
-        onChange={handleChange}
-        placeholder="town/city"
+				onChange={handleChange}
+				placeholder="town/city"
 			/>
-      <label htmlFor="county">County</label>
+			<label htmlFor="county">County</label>
 			<input
 				id="county"
 				value={newContact.county}
 				name="county"
 				type="text"
-        onChange={handleChange}
-        placeholder="county"
+				onChange={handleChange}
+				placeholder="county"
 			/>
 			<label htmlFor="postcode">Postcode</label>
 			<input
 				id="postcode"
-				value={postcode}
+				value={newContact.postcode}
 				name="postcode"
 				type="text"
-        onChange={handleChange}
-        placeholder="postcode"
+				onChange={handleChange}
+				placeholder="postcode"
+			/>
+			<label htmlFor="telephone">Telephone</label>
+			<input
+				id="telephone"
+				value={newContact.telephone}
+				name="telephone"
+				type="text"
+				onChange={handleChange}
+				placeholder="telephone"
+			/>
+			<label htmlFor="email">Email</label>
+			<input
+				id="email"
+				value={newContact.email}
+				name="email"
+				type="text"
+				onChange={handleChange}
+				placeholder="email"
 			/>
 			<button>Add contact</button>
 		</form>
