a623320ee2026a9e5c95f7f0f81890cbe2d8d7da
christocarr <christocarr@outlook.com>
2020-10-22 08:22:01 +0100

    Feature: add react-select to handle postcode search and select

--- src/components/PostCodeSearch.js
+++ src/components/PostCodeSearch.js
@@ -1,51 +1,42 @@
-import React, { useState } from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { selectedAddress } from "./actions";
+import AsyncSelect from "react-select/async";
 
-function PostCodeSearch({ setAddressSelect, setPostcode }) {
-	const [search, setSearch] = useState('');
-	const [addresses, setAddresses] = useState([]);
+function PostcodeSearch(props) {
+  const getAddresses = async (postcode) => {
+    props.setPostcodeSearch(postcode.toUpperCase());
+    const APIKEY1 = "HF5s4vq7pEupvXLhw7uCWg28754"; //paid
+    const APIKEY2 = "HVgKKzu4o0as4MlYxN64XA28757";
+    const APIKEY3 = "c-XX_NRqWkqslKxlwPVUdQ28807";
+    const res = await fetch(
+      `https://api.getAddress.io/find/${postcode}?api-key=${APIKEY1}`
+    );
+    const data = await res.json();
+    const options = data.addresses.map((v) => ({ label: v, value: v }));
+    return options;
+  };
 
-	// fetch addresses from api on postcode form submit
-	function handleSubmit(e) {
-		e.preventDefault();
+  const handleAddressSelect = (selected) => {
+    props.selectedAddress(selected);
+  };
 
-		const APIKEY1 = 'HF5s4vq7pEupvXLhw7uCWg28754';
-		const APIKEY2 = 'HVgKKzu4o0as4MlYxN64XA28757';
-
-		const getAddresses = async () => {
-			let res = await fetch(
-				`https://api.getAddress.io/find/${search}?api-key=${APIKEY1}`
-			);
-			let data = await res.json();
-			//set addresses to addresses array
-			setAddresses(data.addresses);
-		};
-
-		getAddresses();
-		//pass postcode to add contact component
-		setPostcode(search);
-	}
-
-	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<input
-					value={search}
-					onChange={(e) => setSearch(e.target.value)}
-					type="text"
-				/>
-				<button>Search</button>
-				{/* only when address array is not empty display select with option from address array */}
-				{addresses.length > 0 && (
-					<select onChange={(e) => setAddressSelect(e.target.value)}>
-						<option value="">select your address</option>
-						{addresses.map((v, i) => (
-							<option key={i}>{v}</option>
-						))}
-					</select>
-				)}
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <AsyncSelect
+        loadOptions={getAddresses}
+        placeholder="search for postcode"
+        onChange={handleAddressSelect}
+        filterOption={false}
+      />
+    </div>
+  );
 }
 
-export default PostCodeSearch;
+const mapStateToProps = (state) => {
+  return { selectedAddress: state.selectedAddress };
+};
+
+export default connect(mapStateToProps, {
+  selectedAddress
+})(PostcodeSearch);
