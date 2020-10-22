d313fd6e8b5ddf55d3b3313cf4b8687fd13d5c54
christocarr <christocarr@outlook.com>
2020-10-22 13:35:44 +0100

    Add new action add contact

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
 //action creator
 export const selectedAddress = (address) => {
-  return {
-    type: "ADDRESS_SELECTED",
-    payload: address
-  };
+	return {
+		type: 'ADDRESS_SELECTED',
+		payload: address,
+	};
+};
+
+export const addContact = (contact) => {
+	return {
+		type: 'NEW_CONTACT',
+		payload: contact,
+	};
 };
