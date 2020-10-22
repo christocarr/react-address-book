c8653a13a3446e91e4ad05254eebc6537d2d2075
christocarr <christocarr@outlook.com>
2020-10-22 13:36:39 +0100

    Refactor: use redux store state

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import EditContact from './EditContact'
-import DeleteContact from './DeleteContact'
+import EditContact from './EditContact';
+import DeleteContact from './DeleteContact';
 
-//destructure props and render 
-function ContactCard({
-	name,
-	addressLineOne,
-	addressLineTwo,
-	city,
-	county,
-	postcode,
-	telephone,
-	email,
-}) {
+//destructure props and render
+function ContactCard({ contact }) {
+	console.log(contact);
 	return (
 		<div>
-		<address>
-			<h3>{name}</h3>
-			<p>{addressLineOne}</p>
-			<p>{addressLineTwo}</p>
-			<p>{city}</p>
-			<p>{county}</p>
-			<p>{postcode}</p>
-			<p>{telephone}</p>
-			<p>{email}</p>
-		</address>
+			<address>
+				<h3>{contact.name}</h3>
+				<p>
+					{contact.address.firstLine}
+					<br />
+					{contact.address.secondLine}
+					<br />
+					{contact.address.town}
+					<br />
+					{contact.address.county}
+					<br />
+					{contact.address.postcode}
+					<br />
+					{contact.telephone}
+					<br />
+					{contact.email}
+				</p>
+			</address>
 			<EditContact />
 			<DeleteContact />
 		</div>
