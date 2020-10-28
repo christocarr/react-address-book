a4f7bf8b124ddad6028525fee399ef17cc2dbb29
christocarr <christocarr@outlook.com>
2020-10-28 14:39:12 +0000

    Refactor: remove unused modules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import {createStore} from 'redux'
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
-import reducers from './reducers'
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
-
