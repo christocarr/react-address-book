d16a14e36d63f5825bda6cbfa61c6ad7b348ac39
christocarr <christocarr@outlook.com>
2020-11-02 15:53:23 +0000

    Feature: add contact only if contact doesn't exist

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 function rootReducer(state = [], action) {
 	if (action.type === 'ADD_CONTACT') {
-		return [...state, action.payload];
+		//check for existing email already in state
+		const contactExists = state.some(
+			(contact) => contact.email === action.payload.email
+		);
+		if (!contactExists) {
+			return [...state, action.payload];
+		}
 	}
 
 	if (action.type === 'DELETE_CONTACT') {
