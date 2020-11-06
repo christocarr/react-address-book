b0228e38aca208976dcdf9bca342c24d214fdd70
christocarr <christocarr@outlook.com>
2020-11-06 18:35:39 +0000

    Refactor: use switch statement

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,49 +5,84 @@ const initialState = {
 };
 
 function rootReducer(state = initialState, action) {
-	if (action.type === 'ADD_CONTACT') {
-		//check for existing email already in state
-		const contactExists = state.contactList.some(
-			(contact) => contact.email === action.payload.email
-		);
-		if (!contactExists) {
+	switch (action.type) {
+		case 'ADD_CONTACT':
+			//check for existing email already in state
+			const contactExists = state.contactList.some(
+				(contact) => contact.email === action.payload.email
+			);
+			if (!contactExists) {
+				return Object.assign({}, state, {
+					contactList: state.contactList.concat(action.payload),
+				});
+			} else {
+				return state
+			}
+		case 'DELETE_CONTACT':
+			return {
+				...state,
+				contactList: state.contactList.filter(
+					(contact) => contact.email !== action.payload.contact
+				),
+			};
+		case 'TOGGLE_EDIT_CONTACT':
+			return {
+				...state,
+				toggleEditContact: !state.toggleEditContact,
+			};
+		case 'UPDATE_CONTACT':
 			return Object.assign({}, state, {
-				contactList: state.contactList.concat(action.payload),
+				contactList: state.contactList.map((contact) =>
+					contact.date === action.payload.date ? action.payload : contact
+				),
 			});
-		}
+		default:
+			return state;
 	}
-
-	if (action.type === 'DELETE_CONTACT') {
-		return {
-			...state,
-			contactList: state.contactList.filter(
-				(contact) => contact.email !== action.payload.contact
-			),
-		};
-	}
-
-	if (action.type === 'TOGGLE_EDIT_CONTACT') {
-		return {
-			...state,
-			toggleEditContact: !state.toggleEditContact,
-		};
-	}
-
-	if (action.type === 'TOGGLE_MODAL') {
-		return {
-			...state,
-			showModal: !state.showModal
-		}
-	}
-
-	if (action.type === 'UPDATE_CONTACT') {
-		return Object.assign({}, state, {
-			contactList: state.contactList.map((contact) =>
-				contact.date === action.payload.date ? action.payload : contact
-			),
-		});
-	}
-	return state;
 }
+// 	if (action.type === 'ADD_CONTACT') {
+// 		//check for existing email already in state
+// 		const contactExists = state.contactList.some(
+// 			(contact) => contact.email === action.payload.email
+// 		);
+// 		if (!contactExists) {
+// 			return Object.assign({}, state, {
+// 				contactList: state.contactList.concat(action.payload),
+// 			});
+// 		}
+// 	}
+
+// 	if (action.type === 'DELETE_CONTACT') {
+// 		return {
+// 			...state,
+// 			contactList: state.contactList.filter(
+// 				(contact) => contact.email !== action.payload.contact
+// 			),
+// 		};
+// 	}
+
+// 	if (action.type === 'TOGGLE_EDIT_CONTACT') {
+// 		return {
+// 			...state,
+// 			toggleEditContact: !state.toggleEditContact,
+// 		};
+// 	}
+
+// 	if (action.type === 'TOGGLE_MODAL') {
+// 		return {
+// 			...state,
+// 			showModal: !state.showModal,
+// 		};
+// 	}
+
+// 	if (action.type === 'UPDATE_CONTACT') {
+// 		return Object.assign({}, state, {
+// 			contactList: state.contactList.map((contact) =>
+// 				contact.date === action.payload.date ? action.payload : contact
+// 			),
+// 		});
+// 	}
+// 	return state;
+// }
 
 export default rootReducer;
