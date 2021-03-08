19caff83a69a27ed661fd57df95b1f6e7d00d65f
christocarr <christocarr@outlook.com>
2021-03-08 07:50:36 +0000

    feat: button that open and closes new contact form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import ContactForm from './components/ContactForm'
+import React, { useState } from 'react';
+import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
-import '../src/index.css'
+import OpenFormButton from './components/OpenFormButton';
+import '../src/index.css';
 
 function App() {
+  const [displayForm, setDisplayForm] = useState(false);
+  return (
+    <div className="App">
+      <header>
+        <h1>Address Book</h1>
+      </header>
+      <main>
+        <OpenFormButton
+          displayForm={displayForm}
+          setDisplayForm={setDisplayForm}
+        />
+        {displayForm && <ContactForm />}
 
-	return (
-		<div className="App">
-			<header>
-				<h1>Address Book</h1>
-			</header>
-			<main>
-				<ContactForm />
-				<ContactList />
-			</main>
-		</div>
-	);
+        <ContactList />
+      </main>
+    </div>
+  );
 }
 
 export default App;
