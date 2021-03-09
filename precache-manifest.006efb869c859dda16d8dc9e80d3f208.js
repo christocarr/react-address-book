856c7e5ffd0268def73370567f26e2c909a494b0
christocarr <christocarr@outlook.com>
2021-03-09 13:47:27 +0000

    Updates

---
+++ precache-manifest.006efb869c859dda16d8dc9e80d3f208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b553269e8b66e36c18f5ca4a08f573ad",
+    "url": "/react-address-book/index.html"
+  },
+  {
+    "revision": "2dd2346208c58fdf79dc",
+    "url": "/react-address-book/static/css/main.57d51513.chunk.css"
+  },
+  {
+    "revision": "8c501caa7487603c6571",
+    "url": "/react-address-book/static/js/2.48b423fa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-address-book/static/js/2.48b423fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd2346208c58fdf79dc",
+    "url": "/react-address-book/static/js/main.b7ada50c.chunk.js"
+  },
+  {
+    "revision": "d002e18187cb36dede61",
+    "url": "/react-address-book/static/js/runtime-main.337830c1.js"
+  }
+]);
