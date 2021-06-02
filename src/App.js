f687293ebfd467d9cff9bd7187f3bc265a8b0e4d
christocarr <christocarr@outlook.com>
2021-06-02 11:56:42 +0100

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import OpenFormButton from './components/OpenFormButton';
-import '../src/index.css';
 
 function App() {
   const [displayForm, setDisplayForm] = useState(false);
